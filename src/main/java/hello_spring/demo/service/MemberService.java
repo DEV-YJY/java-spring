7880dbf80fc080b2eac2dd76c81640148c9b10c0
YJ You <yyou017@gmail.com>
2025-01-02 13:40:19 +1300

    feat: create MemberService

---
+++ src/main/java/hello_spring/demo/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello_spring.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello_spring.demo.domain.Member;
+import hello_spring.demo.repository.MemberRepository;
+import hello_spring.demo.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+	/**
+	 * member sign in
+	 */
+	public Long join(Member member)	{
+		validateExistingMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateExistingMember(Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {
+				throw new IllegalStateException("The member already exists");
+			});
+	}
+
+	/**
+	 * view all members
+	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	public Optional<Member> findOne(Long id) {
+		return memberRepository.findById(id);
+	}
+}
