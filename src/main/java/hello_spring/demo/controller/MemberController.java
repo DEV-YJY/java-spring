ee6711627388427417fb8ff85fafb087433a341c
YJ You <yyou017@gmail.com>
2025-01-03 13:44:00 +1300

    feat: view member list

--- src/main/java/hello_spring/demo/controller/MemberController.java
+++ src/main/java/hello_spring/demo/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello_spring.demo.controller;
 
+import java.util.List;
+
 import hello_spring.demo.domain.Member;
 import hello_spring.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,5 +32,12 @@ public class MemberController {
 		 memberService.join(member);
 
 		 return "redirect:/";
+	 }
+
+	@GetMapping("/members")
+	public String list(Model model) {
+		 List<Member> members = memberService.findMembers();
+		 model.addAttribute("members", members);
+		 return "members/memberList";
 	 }
 }
