2de02f1eb195471b70b6e60d44c923b287f29dfa
YJ You <yyou017@gmail.com>
2025-01-04 11:50:39 +1300

    feat: create member repo with Jdbc template

--- src/main/java/hello_spring/demo/SpringConfig.java
+++ src/main/java/hello_spring/demo/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello_spring.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello_spring.demo.repository.JdbcTemplateMemberRepository;
 import hello_spring.demo.repository.MemberRepository;
 import hello_spring.demo.repository.MemoryMemberRepository;
 import hello_spring.demo.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+	private final DataSource dataSource;
+
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
